e55013679755ee786a1c7f82f8f490e8535111de
jpemberthy <jpemberthy@github.com>
2022-02-01 10:05:28 -0800

    Update sync when no files have changed

--- watcher.sh
+++ watcher.sh
@@ -1,3 +1,9 @@
 cd ~/github/github
 FLIST=$(git status -s | awk '{print $2}')
-echo "$FLIST" | rsync -arz --files-from=- --no-dirs --whole-file --verbose . github-codespaces:/workspaces/github
+
+if [ -z "$FLIST" ]
+then
+    ssh github-codespaces "cd /workspaces/github; git add .; git reset --hard;"
+else
+    echo "$FLIST" | rsync -arz --files-from=- --no-dirs --whole-file --verbose . github-codespaces:/workspaces/github
+fi
