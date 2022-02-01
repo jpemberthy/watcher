a3b0f3c6b608d85c1a0d543b020aae1564a9fa19
jpemberthy <jpemberthy@github.com>
2022-02-01 11:29:37 -0800

    Always git cleanup prior to rsync'n

--- watcher.sh
+++ watcher.sh
@@ -1,9 +1,11 @@
 cd ~/github/github
 FLIST=$(git status -s | awk '{print $2}')
+current_branch=`git branch | grep \* | awk '{print $2}'`
+ssh github-codespaces "cd /workspaces/github; git add .; git reset --hard; git pull origin $current_branch"
 
 if [ -z "$FLIST" ]
 then
-    ssh github-codespaces "cd /workspaces/github; git add .; git reset --hard;"
+    echo "nothing to sync"
 else
     echo "$FLIST" | rsync -arz --files-from=- --no-dirs --whole-file --verbose . github-codespaces:/workspaces/github
 fi
